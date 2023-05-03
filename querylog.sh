3d41bd4c81765c2e20b0295cc3c02a9b154a207a
Jakob Schlyter <jakob@kirei.se>
2023-05-03 09:45:37 +0200

    capture DoT

--- querylog.sh
+++ querylog.sh
@@ -2,4 +2,4 @@
 
 docker compose exec -u root chrome apt-get update
 docker compose exec -u root chrome apt-get -y install tcpdump
-docker compose exec -u root chrome tcpdump -n -s0 dst port 53
+docker compose exec -u root chrome tcpdump -n -s0 dst port 53 or port 853
