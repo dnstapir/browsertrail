2199e1ac10959c0401ad740050b807c72c197fad
Jakob Schlyter <jakob@kirei.se>
2023-05-03 09:32:33 +0200

    drop iproute2

--- querylog.sh
+++ querylog.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker compose exec -u root chrome apt-get update
-docker compose exec -u root chrome apt-get -y install iproute2 tcpdump
+docker compose exec -u root chrome apt-get -y install tcpdump
 docker compose exec -u root chrome tcpdump -n -s0 dst port 53
